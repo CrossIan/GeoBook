bad4e52c4a01f4eb9fe1f71da1ebc8d1ac234426
rwolfarth <wolfarth.7@osu.edu>
2014-06-03 15:56:20 -0400

    Test commit

---
+++ src/com/cse/geobook/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.cse.geobook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+	}
+}
