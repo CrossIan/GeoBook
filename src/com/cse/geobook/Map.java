02ff7532aef6c87423769d64f61fbad13a33fdc1
ranefer <sul_ranefer@hotmail.com>
2014-06-04 09:30:10 -0400

    Map Created, Map intent created in splash screen, Map permissions created in manifest

---
+++ src/com/cse/geobook/Map.java
@@ -0,0 +1,13 @@
+package com.cse.geobook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Map extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map);
+	}
+}
--- src/com/cse/geobook/SplashScreen.java
+++ src/com/cse/geobook/SplashScreen.java
@@ -3,8 +3,6 @@ package com.cse.geobook;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 
 public class SplashScreen extends Activity {
@@ -14,24 +12,21 @@ public class SplashScreen extends Activity {
 	protected int timeIncrement = 100;
 	protected int sleepTime = 100;
 
-
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
 
-		// Thread for displaying the splash screen	
+		// Thread for displaying the splash screen
 		Thread splashThread = new Thread() {
 			@Override
 			public void run() {
 				try {
 					/*
-					 * This section periodically checks if time has run out.
-					 * 	It checks every 100ms to save resources. The splash
-					 * 	screen is terminated when time runs out of the user
-					 * 	taps the screen.
+					 * This section periodically checks if time has run out. It
+					 * checks every 100ms to save resources. The splash screen
+					 * is terminated when time runs out of the user taps the
+					 * screen.
 					 */
 					int elapsedTime = 0;
 					while (active && (elapsedTime < splashTime)) {
@@ -42,23 +37,18 @@ public class SplashScreen extends Activity {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} finally {
-					// Finish and go to the map activity
 					finish();
-//					startActivity(new Intent(SplashScreen.this,
-//							ConverterActivity.class));
+					// TODO: change to .Login
+					startActivity(new Intent("com.cse.geobook.Map"));
+
 				}
 			}
 		};
 		splashThread.start();
 	}
-	
-	
-	
-	
-	
+
 	/*
-	 * If touched before the count down is complete, move to
-	 * 	the next screen.
+	 * If touched before the count down is complete, move to the next screen.
 	 */
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
