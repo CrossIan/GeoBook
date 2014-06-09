940b6cc11211b535d26a13e07bbbe0881647224f
Nate <Nate@Sul>
2014-06-09 14:36:35 -0400

    test map markers

--- src/com/cse/geobook/Login.java
+++ src/com/cse/geobook/Login.java
@@ -1,13 +1,14 @@
 package com.cse.geobook;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class Login extends Activity {
 
@@ -15,24 +16,27 @@ public class Login extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
-		
-		
-		
+
 		final Button button = (Button) findViewById(R.id.signButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	finish();
-                // Move to the map viewer
-            	startActivity(new Intent("android.intent.action.MAP"));
-            }
-        });
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+				// Move to the map viewer
+				Intent map = new Intent("android.intent.action.MAP");
+
+				Bundle extra = new Bundle();
+				// test data for map
+				ArrayList<LatLng> t = new ArrayList<LatLng>();
+				t.add(new LatLng(-37.81319, 144.96298));
+				t.add(new LatLng(-33.87365, 151.20689));
+				t.add(new LatLng(-34.87365, 152.20689));
+
+				map.putParcelableArrayListExtra("caches", t);
+				// map.putExtra("caches", extra);
+
+				startActivity(map);
+			}
+		});
 
 	}
-	
-	
-	
-	
-	
-	
-	
 }
--- src/com/cse/geobook/Map.java
+++ src/com/cse/geobook/Map.java
@@ -1,5 +1,6 @@
 package com.cse.geobook;
 
+import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -9,17 +10,31 @@ import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class Map extends FragmentActivity {
+
+	GoogleMap gMap;
+	ArrayList<LatLng> caches;
+	Bundle extras;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
+
+		gMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+		caches = getIntent().getParcelableArrayListExtra("caches");
+
+		setUpMap();
 	}
-	
-	
-	
-	
+
 	/*
 	 * Populates the map view menu
 	 */
@@ -31,39 +46,67 @@ public class Map extends FragmentActivity {
 		return true;
 	}
 
-	
-	
-	
 	/*
 	 * Responds to touch events
 	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-		    case R.id.menu_map_profile:
-		    	startActivity(new Intent(Map.this,
-						Profile.class));
-	            return true;
-		    case R.id.menu_map_settings:
-	            return true;
-	        case R.id.menu_map_about:
-	        	AlertDialog.Builder builder = new AlertDialog.Builder(Map.this);
-	        	builder.setMessage(R.string.dialog_about_message)
-	        	       .setTitle(R.string.dialog_about_title);
-	        	builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-	                public void onClick(DialogInterface dialog, int id) {
-	                    // User clicked OK button
-	                }
-	            });
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_map_profile:
+			startActivity(new Intent(Map.this, Profile.class));
+			return true;
+		case R.id.menu_map_settings:
+			return true;
+		case R.id.menu_map_about:
+			AlertDialog.Builder builder = new AlertDialog.Builder(Map.this);
+			builder.setMessage(R.string.dialog_about_message).setTitle(
+					R.string.dialog_about_title);
+			builder.setPositiveButton(R.string.ok,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							// User clicked OK button
+						}
+					});
 
-	        	AlertDialog dialog = builder.create();
-	        	dialog.show();
-	            return true;
-	        case R.id.menu_map_signout:
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			return true;
+		case R.id.menu_map_signout:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
+
+	public void setUpMap() {
+
+		for (int i = 0; i < caches.size(); i++) {
+			addMarker(caches.get(i));
+		}
+
+		gMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+
+			@Override
+			public boolean onMarkerClick(Marker m) {
+				// TODO Auto-generated method stub
+				if (m.isInfoWindowShown()) {
+					m.hideInfoWindow();
+				} else {
+					m.showInfoWindow();
+				}
+
+				return false;
+			}
+
+		});
+
+	}
+
+	private void addMarker(LatLng location) {
+		Marker m = gMap.addMarker(new MarkerOptions().position(location));
+		m.setTitle(""); // or add in MarkerOptions
+		m.setSnippet("");
+	}
+
 }
