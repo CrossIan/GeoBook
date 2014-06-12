72287ab2011b95b7a60135e796b301dcab30439a
IanCross <cross.395@buckeyemail.osu.edu>
2014-06-12 13:43:36 -0400

    Login change

--- src/com/cse/geobook/Login.java
+++ src/com/cse/geobook/Login.java
@@ -3,6 +3,8 @@ package com.cse.geobook;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,22 +14,60 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class Login extends Activity {
 
+	private static final int DIALOG_ALERT = 10;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
+		this.setContentView(R.layout.login);
 
-		final Button button = (Button) findViewById(R.id.signButton);
+		final Button button = (Button) this.findViewById(R.id.signButton);
 		button.setOnClickListener(new View.OnClickListener() {
+			@Override
 			public void onClick(View v) {
-				finish();
-				// Move to the map viewer
-				Intent map = new Intent("android.intent.action.MAP");
-				Bundle extra = new Bundle();
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				        Login.this);
 
-				setDataToPass(extra);
-				map.putExtras(extra);
-				startActivity(map);
+				// set title
+				alertDialogBuilder.setTitle("Sign in");
+
+				// set dialog message
+				alertDialogBuilder
+				        .setMessage("Click yes to exit!")
+				        .setCancelable(false)
+				        .setPositiveButton("Yes",
+				                new DialogInterface.OnClickListener() {
+					                @Override
+					                public void onClick(DialogInterface dialog,
+					                        int id) {
+						                // Move to the map viewer
+						                Intent map = new Intent(
+						                        "android.intent.action.MAP");
+						                Bundle extra = new Bundle();
+
+						                Login.this.setDataToPass(extra);
+						                map.putExtras(extra);
+						                Login.this.startActivity(map);
+						                Login.this.finish();
+					                }
+				                })
+				        .setNegativeButton("No",
+				                new DialogInterface.OnClickListener() {
+					                @Override
+					                public void onClick(DialogInterface dialog,
+					                        int id) {
+						                // if this button is clicked, just close
+						                // the dialog box and do nothing
+						                dialog.cancel();
+					                }
+				                });
+
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+
+				// show it
+				alertDialog.show();
+
 			}
 		});
 
@@ -39,8 +79,8 @@ public class Login extends Activity {
 		// markers (caches)
 		ArrayList<LatLng> data = new ArrayList<LatLng>();
 
-		DataParser reader = new DataParser(getApplicationContext(),
-				R.raw.test_data);
+		DataParser reader = new DataParser(this.getApplicationContext(),
+		        R.raw.test_data);
 		/*
 		 * data.add(new LatLng(39.901138, -82.951465)); data.add(new
 		 * LatLng(39.901138, -83.001465)); data.add(new LatLng(39.901138,
