46ccdab5315038c7701ce132c3d9cb5388011a89
Nate <Nate@Sul>
2014-06-23 18:38:36 -0400

    cache persistent, results on restart

--- src/com/cse/geobook/Cache.java
+++ src/com/cse/geobook/Cache.java
@@ -2,10 +2,12 @@ package com.cse.geobook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -14,8 +16,8 @@ public class Cache extends Activity {
 	LinearLayout view;
 	EditText cacheName;
 	EditText description;
-	LatLng position;
-	MarkerOptions mo;
+	Button save;
+	Data data;
 
 	// PHOTO
 
@@ -24,19 +26,43 @@ public class Cache extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cache);
+		save = (Button) findViewById(R.id.save);
+
 		cacheName = (EditText) findViewById(R.id.cacheName);
+
 		description = (EditText) findViewById(R.id.cacheDescription);
 		getExtras();
+		save.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				int size = data.data.size();
+				boolean searching = true;
+				int i = 0;
+				while (searching && i < size) {
+					MarkerOptions temp = data.data.get(i);
+					if (temp.getPosition().latitude == data.target
+							.getPosition().latitude
+							&& temp.getPosition().longitude == data.target
+									.getPosition().longitude) {
+						temp.title(cacheName.getText().toString());
+						temp.snippet(description.getText().toString());
+						searching = false;
+					}
+					i++;
+				}
+
+				DataParser.overwriteAll(data, getApplicationContext());
+			}
+		});
 	}
 
 	private void getExtras() {
 		Bundle extras = getIntent().getExtras();
-		Data data = extras.getParcelable(Data.CACHE_DATA);
-		mo = data.target;
-		cacheName.setText(mo.getTitle());
-		description.setText(mo.getSnippet());
-		position = mo.getPosition();
+		data = extras.getParcelable(Data.CACHE_DATA);
+		cacheName.setText(data.target.getTitle());
+		description.setText(data.target.getSnippet());
 
 	}
 
@@ -47,4 +73,5 @@ public class Cache extends Activity {
 		b.putDouble("Lat", marker.getPosition().latitude);
 		b.putDouble("Lng", marker.getPosition().longitude);
 	}
+
 }
