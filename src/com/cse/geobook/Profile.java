d77997fec9afd1382b8009bea25b8fdaac15e6f6
ranefer <sul_ranefer@hotmail.com>
2014-07-08 14:16:56 -0400

    Profile progress updated

--- src/com/cse/geobook/Profile.java
+++ src/com/cse/geobook/Profile.java
@@ -4,12 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class Profile extends Activity {
 
+	ProgressBar progress;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profile);
+		progress = (ProgressBar) findViewById(R.id.cacheprogbar);
+
+		this.setProgress(35, 100);
+		
+	}
+	private void setProgress(int progress, int max) {
+		this.progress.setProgress(progress);
+		this.progress.setMax(max);
 	}
 }
