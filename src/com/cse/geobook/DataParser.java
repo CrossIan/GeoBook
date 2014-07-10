d80d8f9cf85641494f1d0e4e5c5174e6e3408f52
rwolfarth <wolfarth.7@osu.edu>
2014-07-10 00:50:57 -0400

    Fixed DataParser. Labels appear on markers again...

--- src/com/cse/geobook/DataParser.java
+++ src/com/cse/geobook/DataParser.java
@@ -102,7 +102,7 @@ public class DataParser {
 		String line;
 		try {
 			line = this.reader.readLine();
-			String[] contents = line.split(",");
+			String[] contents = line.split("[,]");
 //			Log.d("RW", Integer.toString(contents.length));
 
 			// if line matches required format
@@ -112,8 +112,8 @@ public class DataParser {
 
 				Double lat = Double.parseDouble(contents[0]);
 				Double lng = Double.parseDouble(contents[1]);
-//				String title = contents[2];
-				String title = "Temp Title";
+				String title = contents[2];
+//			d	String title = "Temp Title";
 				String description = "";
 				for (int i = 3; i < contents.length; i++) {
 					description += contents[3];
